6a32bd3199aad5ec64b6079de44da22a77722d49
Krocero01 <ale_c1@hotmail.com>
2024-02-12 00:22:51 -0300

    Porfolio angular correcto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./componentes/nav/nav.component";
+import { EncabezadoComponent } from "./componentes/encabezado/encabezado.component";
+import { AcercaDeComponent } from "./componentes/acerca-de/acerca-de.component";
+import { ExperienciaComponent } from "./componentes/experiencia/experiencia.component";
+import { EducacionComponent } from "./componentes/educacion/educacion.component";
+import { SkillsComponent } from "./componentes/skills/skills.component";
+import { ProyectosComponent } from "./componentes/proyectos/proyectos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavComponent, EncabezadoComponent, AcercaDeComponent, ExperienciaComponent, EducacionComponent, SkillsComponent, ProyectosComponent]
 })
 export class AppComponent {
   title = 'Porfolio-Angular';
